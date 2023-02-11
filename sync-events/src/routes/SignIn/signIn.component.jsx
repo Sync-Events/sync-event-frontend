cf3095b6e2667d94031d4c36bd4a40a1c8b545de
sachdevavaibhav <sachdevavaibhav.2001@gmail.com>
2023-02-11 18:49:36 +0530

    controlled sign-in form

--- sync-events/src/routes/SignIn/signIn.component.jsx
+++ sync-events/src/routes/SignIn/signIn.component.jsx
@@ -1,39 +1,54 @@
+import { useState } from 'react'
 import './signIn.styles.css'
 
 function SignIn() {
+
+    const [formData, setFormData] = useState({
+        email: "",
+        password: "",
+    }) 
+
+    const handleChange = (event) => {
+        setFormData({
+            ...formData,
+            [event.target.name]: event.target.value,
+        })
+        console.dir(event.target)
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        console.log(formData)
+    }
+
     return(
         <div className="conatiner-main">
             <div className="wrapperme">
                 <div className="titlenew">
-                Login Form
+                    Login Form
                 </div>
-                <form action="#">
-                <div className="field">
-                    <input type="text" required/>
-                    <label>Email Address</label>
-                </div>
-                <div className="field">
-                    <input type="password" required/>
-                    <label>Password</label>
-                </div>
-                <div className="contentnew">
-                    <div className="checkbox">
-                        <input type="checkbox" id="remember-me"/>
-                        <label for="remember-me">Remember me</label>
+                <form onSubmit={handleSubmit}>
+                    <div className="field">
+                        <input type="text" name="email" onChange={handleChange} value={formData.email} id="email" required/>
+                        <label htmlFor="email">Email Address</label>
                     </div>
-                    <div className="pass-link">
-                        <a href="/">Forgot password?</a>
+                    <div className="field">
+                        <input type="password" name="password" onChange={handleChange} value={formData.password} id="password" required/>
+                        <label htmlFor="password">Password</label>
                     </div>
-                </div>
-                <div className="field">
-                    <input type="submit" value="Login"/>
-                </div>
-                <div className="signup-link">
-                    Not a member? <a href="/">Signup now</a>
-                </div>
+                    <div className="contentnew">
+                        <div className="pass-link">
+                            <a href="/">Forgot password?</a>
+                        </div>
+                    </div>
+                    <div className="field">
+                        <input type="submit" value="Login"/>
+                    </div>
+                    <div className="signup-link">
+                        Not a member? <a href="/">Signup now</a>
+                    </div>
                 </form>
             </div>
-
         </div>
     )
 }
