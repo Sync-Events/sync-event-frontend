716b0b4937d2e43818891bfa67bcd087d738fa72
theteamisbest <abhithory@gmail.com>
2023-02-12 07:42:13 +0530

    profile fixed

--- src/components/RegisteredCard/RegisteredCard.component.jsx
+++ src/components/RegisteredCard/RegisteredCard.component.jsx
@@ -2,9 +2,7 @@ function RegisteredCard(data) {
     return(
         <div className="card" >
         <ul className="list-group list-group-flush">
-            {data}
-            <a className="list-group-item" href={data}>{data}</a>
-            <a className="list-group-item">A second item</a>
+            <a className="list-group-item">data</a>
             <a className="list-group-item">A third item</a>
         </ul>
         </div>
--- src/routes/Profile/Profile.component.jsx
+++ src/routes/Profile/Profile.component.jsx
@@ -6,72 +6,48 @@ import axios from 'axios'
 import RegisteredCard from '../../components/RegisteredCard/RegisteredCard.component';
 
 import "./profile.css";
+import EventCard from '../../components/EventCard/eventCard.component';
 
 
 export default function Profile() {
     
-    // const eventDetail = [{
-    //     banner: 'https://picsum.photos/200',
-    //     eventName: 'Tech Talks',
-    //     societyName: 'TCP',
-    //     eventDescription: 'It is series of seminars and workshops',
-    //     venue: 'KMC, auditorium',
-    //     eventDates: {start: "02 July", end: "03 July"},
-    //     // eventTimes: '12PM',
-    //     contact: [{website:"www.", phoneNo:"9898", email: "vabs"}]
-    // }]
-    const eventDetail = [
-        {
-            id: "1",
-            eventName: "Tech Talks",
-            url: "/"
-        }
-    ]
     const [userDetails, setUserDetails] = useState(null);
     const [events, setEvents] = useState([]);
 
     const loadUserData = async () => {
         const userData = JSON.parse(localStorage.getItem("userData"));
-        console.log(userData);
         const response = await axios.get(`${process.env.REACT_APP_BASE_API_URL}/auth/getUser/${userData.id}`);
-        console.log(response.data.data);
         setUserDetails(response.data.data)
 
-        // if (response.data.data.userType === "Student") {
-        //     const events = await axios.get(`${process.env.REACT_APP_BASE_API_URL}/event/registredByUser`, {
-        //         headers: { Authorization: `Bearer ${userData.token}` }
-        //     });
+        if (response.data.data.userType === "Student") {
+            const events = await axios.get(`${process.env.REACT_APP_BASE_API_URL}/event/registredByUser`, {
+                headers: { Authorization: `Bearer ${userData.token}` }
+            });
 
-        //     console.log("events");
-        //     console.log(events.data.data);
-        //     setEvents(events.data.data);
-        // } else {
-        //     const events = await axios.get(`${process.env.REACT_APP_BASE_API_URL}/event/of/${userData.id}`);
+            console.log("events");
+            console.log(events.data.data);
+            setEvents(events.data.data);
+        } else {
 
-        //     console.log("events");
-        //     console.log(events.data.data);
-        //     setEvents(events.data.data);
-        // }
+            console.log("ahffaslkjfhdskjfh");
+            const events = await axios.get(`${process.env.REACT_APP_BASE_API_URL}/event/ofsociety/${userData.id}`);
+
+            console.log("events");
+            console.log(events.data.data);
+            setEvents(events.data.data);
+        }
     }
 
     useEffect(() => {
-        setEvents(eventDetail)
         loadUserData();
     }, [])
 
 
     return (
-<<<<<<< HEAD
-        <div className=''>
-            <div className="profile-box">
-                {userDetails && <img src={userDetails.image} alt="" />}
-                <div className="profile-info">
-=======
         <div className='profile-container d-flex flex-column justify-content-center align-items-center mt-3'>
             <div className="profile-box mb-5">
                 {userDetails && <img className='image-fluid' src={userDetails.image} alt="" />}
                 <div className="profile-info mt-3">
->>>>>>> dc9a886508600697bbf1bcd5b7c8391a6b0e0656
                     <h4>Name: {userDetails?.fullName}</h4>
                     <div className='profile-details'>
                         {userDetails?.collegeYear && <h4 className='text-muted'>College Year: {userDetails.collegeYear}</h4>}
@@ -84,8 +60,12 @@ export default function Profile() {
                     {userDetails && userDetails.userType === "Student" ? "Registrated Events" : "Published Events"}
                 </h4>
                 <div className="eventsList">
-                    {console.log(events)}
-                    {events.length > 0 ? <RegisteredCard data={events} /> 
+                    {events.length > 0 ? events.map((event,key)=>{
+                        return(
+                            // <RegisteredCard data={event} key={key} />
+                            <EventCard data={event} key={key} />
+                        )
+                    }) 
                         :
                         <h4>No events</h4>
                     }
